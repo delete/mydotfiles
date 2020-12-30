f327bb88a2523f22433e3286e3f817ed73e714e2
Fellipe Pinheiro <pinheiro.llip@gmail.com>
2020-12-30 13:11:02 -0300

    add aliases file

---
+++ aliases.zsh
@@ -0,0 +1,31 @@
+# linux aliasses
+if [[ $OSTYPE == linux-gnu* ]]; then
+
+    alias sysup='sudo pacman -Syu && yaourt -Syyuua --noconfirm'
+    alias clearcache='sudo pacman -Scc --noconfirm && sudo journalctl --vacuum-time=2d && rm -rf ~/.local/share/Trash/files'
+    
+    # Modified Commands
+    alias free='free -m'
+
+# macOS aliasses
+elif [[ $OSTYPE == darwin* ]]; then
+    
+fi
+
+# Remove all node_modules directory
+alias rmnode='find . -name "node_modules" -type d -exec rm -rf '{}' +'
+
+# Docker
+alias drm="docker ps --filter status=dead --filter status=exited -aq |│ xargs docker rm -v"
+alias drmi="docker images --no-trunc | grep '<none>' | awk '{ print $3 │ }' | xargs docker rmi"
+alias dstop='docker stop $(docker ps -a -q)'
+alias drmv='docker volume ls -qf dangling=true | xargs -r docker volume rm'
+alias rmvolumes='cat volumes | while read LINE; do docker volume rm ${LINE}; done'
+
+# Modified Commands
+alias df='df -h'
+alias mkdir='mkdir -p -v'
+alias ll='ls -alh'
+
+#Projects
+alias cdef="cd ~/Projects/easyflow"
